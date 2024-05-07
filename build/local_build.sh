cdc852d9fce6941096523839e4e8244bd9e63867
Sylvain Guernion <gsylvain35@gmail.com>
2024-05-07 10:58:42 +0200

    update rfxcom2mqtt version

--- build/local_build.sh
+++ build/local_build.sh
@@ -1,10 +1,5 @@
 cd ..
-cp build/Dockerfile rfxcom2mqtt-dev/
-cp -R build/rootfs rfxcom2mqtt-dev/
 docker run --rm --privileged \
 -v $(pwd)/rfxcom2mqtt-dev:/data homeassistant/i386-builder --all -t /data \
 --no-cache
 #docker build ./rfxcom2mqtt-dev --build-arg="BUILD_VERSION=dev" -t rfxcom2mqtt/hassio-addon:dev
-
-rm -rf rfxcom2mqtt-dev/rootfs
-rm rfxcom2mqtt-dev/Dockerfile
