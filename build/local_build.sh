08fc37a091473e50142b01ae604c22371a1018e6
Sylvain Guernion <gsylvain35@gmail.com>
2024-05-14 12:56:34 +0200

    build

--- build/local_build.sh
+++ build/local_build.sh
@@ -1,5 +1,3 @@
 cd ..
-docker run --rm --privileged \
--v $(pwd)/rfxcom2mqtt-dev:/data homeassistant/i386-builder --all -t /data \
---no-cache
+docker run --rm --privileged -v $(pwd)/rfxcom2mqtt-dev:/data homeassistant/amd64-builder ---amd64 -t /data --no-cache
 #docker build ./rfxcom2mqtt-dev --build-arg="BUILD_VERSION=dev" -t rfxcom2mqtt/hassio-addon:dev
